b2b00e7f9474471a4aa8a03aaaa826073eea825a
Adam McKenna <adam.mckenna@namics.com>
2018-09-14 16:23:16 +0200

    unit testing: minor changes to 'App' component tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,16 +1,19 @@
-import { shallow } from 'enzyme';
+import { mount, shallow } from 'enzyme';
 import * as React from 'react';
 import App from './App';
 import RootStore from './store/RootStore';
+
 const store: RootStore = new RootStore();
 
-it('renders without crashing', () => {
-    const component = shallow(<App store={store} />);
-    expect(component).toMatchSnapshot();
-});
+describe('App', () => {
+    it('renders without crashing', () => {
+        const component = shallow(<App store={store} />);
+        expect(component).toMatchSnapshot();
+    });
 
-it('should call requestApiDataFromStore method', () => {
-    const spy = jest.spyOn(App.prototype, '_requestApiDataFromStore');
-    shallow(<App store={store} />);
-    expect(spy).toHaveBeenCalled();
+    it('should call requestApiDataFromStore method during componentDidMount', () => {
+        const requestApiDataFromStoreFake = jest.spyOn<any, any>(App.prototype, '_requestApiDataFromStore');
+        mount(<App store={store} />);
+        expect(requestApiDataFromStoreFake).toHaveBeenCalledTimes(1);
+    });
 });
